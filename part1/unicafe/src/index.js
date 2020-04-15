b1f08c2b6e5c621ba32388bd1b20032fa4943796
James Phillips <james@jp2.me.uk>
2020-04-15 19:17:17 +0100

    1.7 and 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -14,34 +14,38 @@ const Statistic = ({name, variable}) => (
   <p>{name} {variable}</p>
 )
 
+const Statistics = ({good, bad, neutral}) => {
+  const calculateAverage = (good, bad, all) => all === 0 ? 0 : (good - bad) / all
+
+  const calculatePositive = (good, all) => all === 0 ? 0 : (good / all) * 100
+
+  const all = good + bad + neutral
+
+  if (all === 0) return <p>No feedback given</p>
+
+  return (
+    <>
+    <Statistic name="good" variable={good} />
+    <Statistic name="neutral" variable={neutral} />
+    <Statistic name="bad" variable={bad} />
+    <Statistic name="all" variable={all} />
+    <Statistic name="average" variable={calculateAverage(good, bad, all)} />
+    <Statistic name="positive" variable={calculatePositive(good, all) + "%"} />
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
 
-  const incrementGood = () => {
-    setGood(good + 1)
-    setAll(all + 1)
-    calculateAverage()
-  }
+  const incrementGood = () => setGood(good + 1)
 
-  const incrementNeutral = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
-    calculateAverage()
-  }
+  const incrementNeutral = () => setNeutral(neutral + 1)
 
-  const incrementBad = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
-    calculateAverage()
-  }
-
-  const calculateAverage = () => setAverage((good - bad) / all)
+  const incrementBad = () => setBad(bad + 1)
 
   return (
     <>
@@ -50,11 +54,7 @@ const App = () => {
       <Button onClick={incrementNeutral} text="neutral" />
       <Button onClick={incrementBad} text="bad" />
       <Header text="statistics" />
-      <Statistic name="good" variable={good} />
-      <Statistic name="neutral" variable={neutral} />
-      <Statistic name="bad" variable={bad} />
-      <Statistic name="all" variable={all} />
-      <Statistic name="average" variable={average} />
+      <Statistics good={good} bad={bad} neutral={neutral}></Statistics>
     </>
   )
 }
