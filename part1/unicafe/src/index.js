b35bb385f8077b458278bdb902b5d17c1a764450
James Phillips <james@jp2.me.uk>
2020-04-15 18:55:16 +0100

    ex1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const Header = ({text}) => (
+  <h1>{text}</h1>
+)
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const Statistic = ({name, variable}) => (
+  <p>{name} {variable}</p>
+)
+
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+
+  return (
+    <>
+      <Header text="give feedback" />
+      <Button onClick={incrementGood} text="good" />
+      <Button onClick={incrementNeutral} text="neutral" />
+      <Button onClick={incrementBad} text="bad" />
+      <Header text="statistics" />
+      <Statistic name="good" variable={good} />
+      <Statistic name="neutral" variable={neutral} />
+      <Statistic name="bad" variable={bad} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
