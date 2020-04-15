5cef3ada47a3de0c53f4b9fcd4ee380c1b275df8
James Phillips <james@jp2.me.uk>
2020-04-15 16:53:55 +0100

    Bits of 1C example

--- part1/example/src/index.js
+++ part1/example/src/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, Your age is {props.age} years.</p>
-    </div>
-  )
-}
+const Hello = ({name, age}) => (
+  <div>
+    <p>Hello {name}, Your age is {age} years.</p>
+  </div>
+)
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   const name = 'James'
-  const age = 10
+  const [ age, setAge] = useState(10)
+
+  const incrementAge = () => setAge(age + 1)
+  const setDefault = () => setAge(10)
+
   return (
     <>
       <h1>Greetrings</h1>
       <Hello name="Will" age={14 + 10}/>
       <Hello name={name} age={age}/>
+      <Button handleClick={incrementAge} text="plus" />
+      <Button handleClick={setDefault} text="default"/>
     </>
   )
 }
