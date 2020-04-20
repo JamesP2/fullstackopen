5761c446cb02316ed296979fd431bceb613821a0
James Phillips <jamesp2.phillips@gmail.com>
2020-04-20 17:20:23 +0100

    2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,32 +1,37 @@
 import React from 'react'
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+import Course from './components/Course'
 
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
+    const courses = 
+    [
+        {
+            id: 1,
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    id: 1,
+                    name: 'Fundamentals of React',
+                    exercises: 10
+                },
+                {
+                    id: 2,
+                    name: 'Using props to pass data',
+                    exercises: 7
+                },
+                {
+                    id: 3,
+                    name: 'State of a component',
+                    exercises: 14
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
+            {courses.map(course =>
+                <Course key={course.id} course={course} />    
+            )}
         </div>
     )
 }
